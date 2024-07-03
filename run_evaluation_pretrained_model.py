a732d3f5dcc768c29549501cfd5c51effe6b5e16
business-research <160495505+business-research@users.noreply.github.com>
2024-07-03 17:27:32 +0800

    Update run_evaluation_pretrained_model.py

--- run_evaluation_pretrained_model.py
+++ run_evaluation_pretrained_model.py
@@ -6,7 +6,7 @@ from moudle_test import  do_test
 import torch
 
 
-def test(dataset_name, feature_path, mode,pretrained_model_path, config_file='config/config_pretrained.json'):
+def test(dataset_name, feature_path,  pretrained_model_path, config_file='config/config_pretrained.json'):
     args = get_config_regression(dataset_name, config_file)
     args.featurePath = feature_path
     args.device = assign_gpu([0])
@@ -18,7 +18,7 @@ def test(dataset_name, feature_path, mode,pretrained_model_path, config_file='co
     model.load_state_dict(torch.load(pretrained_model_path))
     model.to(args.device)
 
-    do_test(args, model, dataloader[mode], mode)
+    do_test(args, model, dataloader["test"])
 
 
 if __name__ == '__main__':
@@ -26,6 +26,4 @@ if __name__ == '__main__':
     feature_path = r"data/Salesperson_Attractiveness_data1993.pkl"
     pretrained_model_path = r"pretrained_model/tfn_pretrained.pth"
 
-    for mode in ["train", "valid", "test"]:
-        print(f"@@ mode: {mode}")
-        test(dataset_name, feature_path, mode,pretrained_model_path)
+    test(dataset_name, feature_path,  pretrained_model_path)
